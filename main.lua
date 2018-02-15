f87140a4e7cea242ce22ba828ba063fe6113ccf4
Amin Zeina <amin.zeina@ocsbstudent.ca>
2018-02-15 13:22:17 -0500

    Final

--- main.lua
+++ main.lua
@@ -2,6 +2,22 @@
 --
 -- main.lua
 --
+-- Created By: Amin Zeina
+-- Created on: Feb 2018
+--
+-- Add an event listener for touch
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local soccerBall = display.newImageRect( "./assets/sprites/ball.png", 500, 480)
+soccerBall.x = display.contentCenterX
+soccerBall.y = display.contentCenterY
+soccerBall.id = ball
+
+local function onObjectTouch( event )
+	-- state that the ball has been touched in console
+    print( "Touched Ball")
+    
+    return true
+end
+
+soccerBall:addEventListener( "touch", onObjectTouch)
